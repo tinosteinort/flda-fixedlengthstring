154ef155682bda35a0a05c929c37043b7b4e722b
Tino Steinort <tinosteinort@live.de>
2018-05-29 21:44:27 +0200

    simplify example code

--- src/test/java/com/github/tinosteinort/flda/fixedlengthstring/fullexample/FixedLengthStringInterfaceTest.java
+++ src/test/java/com/github/tinosteinort/flda/fixedlengthstring/fullexample/FixedLengthStringInterfaceTest.java
@@ -2,6 +2,7 @@ package com.github.tinosteinort.flda.fixedlengthstring.fullexample;
 
 import com.github.tinosteinort.flda.accessor.ReadAccessor;
 import com.github.tinosteinort.flda.accessor.WriteAccessor;
+import com.github.tinosteinort.flda.fixedlengthstring.Alignment;
 import com.github.tinosteinort.flda.fixedlengthstring.FixedLengthString;
 import com.github.tinosteinort.flda.fixedlengthstring.FixedLengthStringAccessorConfig;
 import com.github.tinosteinort.flda.fixedlengthstring.FixedLengthStringAccessorConfigBuilder;
@@ -15,87 +16,75 @@ import com.github.tinosteinort.flda.fixedlengthstring.writer.StringAttributeWrit
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.UnsupportedEncodingException;
-
 public class FixedLengthStringInterfaceTest {
 
-    private final LengthValidator validator = new LengthValidator(PersonDescriptor.LENGTH);
+    private static final int RECORD_LENGTH = 24;
+    private static final FixedLengthStringAttribute<String> FIRST_NAME = new FixedLengthStringAttribute<>(String.class, 0, 10, '.');
+    private static final FixedLengthStringAttribute<String> LAST_NAME = new FixedLengthStringAttribute<>(String.class, 10, 10, '.');
+    private static final FixedLengthStringAttribute<Integer> AGE = new FixedLengthStringAttribute<>(Integer.class, 20, 4, Alignment.RIGHT, '#');
 
     private final FixedLengthStringAccessorConfig config = new FixedLengthStringAccessorConfigBuilder()
             .withDefaultReaders()
             .withDefaultWriters()
-            .withRecordFactory(new FixedLengthStringFactory(PersonDescriptor.LENGTH, ' '))
-            .withReadValidator(validator)
-            .withWriteValidator(validator)
+            .withRecordFactory(new FixedLengthStringFactory(RECORD_LENGTH, '_'))
+            .withReadValidator(new LengthValidator(RECORD_LENGTH))
+            .withWriteValidator(new LengthValidator(RECORD_LENGTH))
             .build();
 
-    @Test public void testImport() throws UnsupportedEncodingException {
+    @Test public void testReadExample() {
 
-        final String dataFromFile = "Dagobert  Duck      100";
+        final String record = "Dagobert..Duck......#100";
 
-        final Person person = doImport(dataFromFile);
+        final ReadAccessor<FixedLengthString, FixedLengthStringAttribute<?>> readAccessor =
+                config.newReadAccessor(new FixedLengthString(record));
+
+        final Person person = new Person();
+        person.setFirstname(readAccessor.read(FIRST_NAME));
+        person.setLastname(readAccessor.read(LAST_NAME));
+        person.setAge(readAccessor.read(AGE));
 
         Assert.assertEquals("Dagobert", person.getFirstname());
         Assert.assertEquals("Duck", person.getLastname());
         Assert.assertEquals(100, person.getAge());
     }
 
-    public Person doImport(final String data) {
-
-        final Person person = new Person();
-
-        final ReadAccessor<FixedLengthString, FixedLengthStringAttribute<?>> readAccessor =
-                config.newReadAccessor(new FixedLengthString(data));
-
-        person.setFirstname(readAccessor.read(PersonDescriptor.FIRST_NAME));
-        person.setLastname(readAccessor.read(PersonDescriptor.LAST_NAME));
-        person.setAge(readAccessor.read(PersonDescriptor.AGE));
-
-        return person;
-    }
-
-    @Test public void testExport() throws UnsupportedEncodingException {
+    @Test public void testWriteExample() {
 
         final Person person = new Person();
         person.setFirstname("Dagobert");
         person.setLastname("Duck");
         person.setAge(100);
 
+        final FixedLengthString generatedRecord = config.createNewRecord();
+
+        final WriteAccessor<FixedLengthString, FixedLengthStringAttribute<?>> writer = config.newWriteAccessor(generatedRecord);
+
+        writer.write(FIRST_NAME, person.getFirstname());
+        writer.write(LAST_NAME, person.getLastname());
+        writer.write(AGE, person.getAge());
+
         Assert.assertEquals(
-                new FixedLengthString("Dagobert  Duck      100"),
-                doExport(person));
-    }
-
-    public FixedLengthString doExport(final Person person) {
-
-        final FixedLengthString row = config.createNewRecord();
-
-        final WriteAccessor<FixedLengthString, FixedLengthStringAttribute<?>> writer = config.newWriteAccessor(row);
-
-        writer.write(PersonDescriptor.FIRST_NAME, person.getFirstname());
-        writer.write(PersonDescriptor.LAST_NAME, person.getLastname());
-        writer.write(PersonDescriptor.AGE, person.getAge());
-
-        return row;
+                new FixedLengthString("Dagobert..Duck......#100"),
+                generatedRecord);
     }
 
     @Test public void testImportCustomAttribute() {
 
         final FixedLengthStringAccessorConfig localConfig = new FixedLengthStringAccessorConfigBuilder(config)
                 // override default behaviour for special attribute:
-                .registerReader(PersonDescriptor.AGE, new AgeDecrementIntegerWriter(1))
+                .registerReader(AGE, new AgeDecrementIntegerWriter(1))
                 .build();
 
-        final String dataFromFile = "Tick      Duck        8";
+        final String dataFromFile = "Tick......Duck......###8";
 
         final Person person = new Person();
 
        final ReadAccessor<FixedLengthString, FixedLengthStringAttribute<?>> readAccessor =
                localConfig.newReadAccessor(new FixedLengthString(dataFromFile));
 
-        person.setFirstname(readAccessor.read(PersonDescriptor.FIRST_NAME));
-        person.setLastname(readAccessor.read(PersonDescriptor.LAST_NAME));
-        person.setAge(readAccessor.read(PersonDescriptor.AGE));
+        person.setFirstname(readAccessor.read(FIRST_NAME));
+        person.setLastname(readAccessor.read(LAST_NAME));
+        person.setAge(readAccessor.read(AGE));
 
         Assert.assertEquals("Tick", person.getFirstname());
         Assert.assertEquals("Duck", person.getLastname());
@@ -106,7 +95,7 @@ public class FixedLengthStringInterfaceTest {
 
         final FixedLengthStringAccessorConfig localConfig = new FixedLengthStringAccessorConfigBuilder(config)
                 // override default behaviour for special attribute
-                .registerWriter(PersonDescriptor.AGE, new AgeIncrementIntegerWriter(2))
+                .registerWriter(AGE, new AgeIncrementIntegerWriter(2))
                 .build();
 
         final Person person = new Person();
@@ -119,12 +108,12 @@ public class FixedLengthStringInterfaceTest {
         final WriteAccessor<FixedLengthString, FixedLengthStringAttribute<?>> writer =
                 localConfig.newWriteAccessor(row);
 
-        writer.write(PersonDescriptor.FIRST_NAME, person.getFirstname());
-        writer.write(PersonDescriptor.LAST_NAME, person.getLastname());
-        writer.write(PersonDescriptor.AGE, person.getAge());
+        writer.write(FIRST_NAME, person.getFirstname());
+        writer.write(LAST_NAME, person.getLastname());
+        writer.write(AGE, person.getAge());
 
         Assert.assertEquals(
-                new FixedLengthString("Tick      Duck        9"),
+                new FixedLengthString("Tick......Duck......###9"),
                 row);
     }
 
@@ -137,15 +126,15 @@ public class FixedLengthStringInterfaceTest {
                 .registerWriter(Integer.class, new IntegerAttributeWriter())
                 .build();
 
-        final String data = "Tick      Duck        7";
+        final String data = "Tick......Duck......###7";
 
         final ReadAccessor<FixedLengthString, FixedLengthStringAttribute<?>> reader =
                 localConfig.newReadAccessor(new FixedLengthString(data));
         final WriteAccessor<FixedLengthString, FixedLengthStringAttribute<?>> writer =
                 localConfig.newWriteAccessor(new FixedLengthString(data));
         // This two Lines of Code does not compile properly if everything works fine
-//        final String age = reader.read(PersonDescriptor.AGE);
-//        writer.write(PersonDescriptor.AGE, "123");
+//        final String age = reader.read(AGE);
+//        writer.write(AGE, "123");
     }
 }
 
@@ -174,5 +163,33 @@ class AgeDecrementIntegerWriter extends IntegerAttributeReader {
     @Override public Integer read(final FixedLengthString data, final FixedLengthStringAttribute<Integer> attribute) {
         final Integer value = super.read(data, attribute);
         return (value == null ? 0 : (value - yearsToDecrement));
+    }
+}
+
+class Person {
+
+    private String firstname;
+    private String lastname;
+    private int age;
+
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
     }
 }
